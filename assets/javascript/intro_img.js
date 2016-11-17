f6936adad9ec6c2fd2b852658680ce1f246bc4bd
Bernardo Martin <bermartinv@gmail.com>
2016-11-17 11:18:07 +0100

    Referencias comienzo

--- assets/javascript/intro_img.js
+++
@@ -1,9 +0,0 @@
-window.onload = function (){
-var item = document.getElementById("intro_img").src;
-if (item == ""){
-    item.style.display="none";
-    
-}
-var hello = document.getElementById("hello");
-    hello.innerHTML = "Hello, how is it going?"    
-}
--- assets/javascript/slides_logo.js
+++ assets/javascript/slides_logo.js
@@ -1,7 +1,13 @@
 
 
 var cont = 0;
-window.onload = function slides_logo(){
+window.onload = function(){
+    slides_logo();
+    titulo();
+}
+    
+    
+function slides_logo(){
     var images = ['assets/images/css3.gif' ,'assets/images/html5.gif' ,'assets/images/javascript.gif' ,'assets/images/grunt.gif' ,'assets/images/github.gif'] 
 var item, repeat;
 var item = document.getElementById("slide");
@@ -12,7 +18,11 @@ var repeat = setTimeout(slides_logo,3000);
 
 }
 
-
+function titulo(){
+    if ((window.innerWidth)<=520){
+    document.getElementsByClassName("name")[0].innerHTML = "Aprende CSS";
+    }
+}
 /*
 
 */
