fc967186dc40cbb4b396043ab20240fc6273a4f1
bermartinv <bermartinv@gmail.com>
2017-02-10 23:34:01 +0100

    Restart border tool

--- assets/javascript/herramienta_border.js
+++ assets/javascript/herramienta_border.js
@@ -16,31 +16,50 @@ function herramienta_border(){
 
 function bordesNormales(){
     mostrar();
+    document.getElementById('redondos').style.display= 'none';
+    document.getElementById('elipses').style.display= 'none';
     var radioColor = document.getElementsByName('color');
-      radioColor[0].addEventListener('click',unColor);
-      radioColor[1].addEventListener('click',cuatroColores);
+    var colorAll = document.getElementsByName('colorAll');
+    var colorFour = document.getElementsByName('colorFour');
+    deshability(colorAll);
+    deshability(colorFour);
+    radioColor[0].addEventListener('click',function(event){unColor(event,colorAll,colorFour);});
+    radioColor[1].addEventListener('click',function(event){cuatroColores(event,colorFour,colorAll);});
+}
+
+function unColor(obj,colorAll,colorFour){
+  deshability(colorFour);
+  hability(colorAll);
+}
+function cuatroColores(obj,colorFour,colorAll){
+  hability(colorFour);
+  deshability(colorAll);
 }
 function bordesCirculares(){
   mostrar();
+  document.getElementById('redondos').style.display= 'block';
+  document.getElementById('elipses').style.display= 'none';
 }
 function bordesElipses(){
   mostrar();
+  document.getElementById('elipses').style.display= 'block';
+  document.getElementById('redondos').style.display= 'none';
 }
-
-
-
 
 /* Utiles para el codigo */
 function hability(vectorElementos){
-  for (var i = 0; i <= vectorElementos.length;i++){
-    vectorElementos[i].disabled = false;
+  for(cadaUno in vectorElementos){
+    vectorElementos[cadaUno].disabled = false;
   }
+
 }
 function deshability(vectorElementos){
-  for(var i = 0;i <= vectorElementos.length; i++){
-    vectorElementos[i].disabled = true;
+  for(cadaUno in vectorElementos){
+    vectorElementos[cadaUno].disabled = true;
   }
 }
+
+
 function mostrar(){
   document.getElementById('color_border').style.display= 'block';
   document.getElementById('estilo_border').style.display= 'block';
